541f93ffc467660db54978836a6a03a8d7878e54
Anton Bossenbroek <anton.bossenbroek@me.com>
2017-01-29 15:43:51 +0100

    Add training labels.

--- model.py
+++ model.py
@@ -14,8 +14,11 @@ def load_images(files, data_path):
 
 
 def build_model(model_path, data_path):
+    # Load the log file.
     drive_log = pd.read_csv("%s/driving_log.csv" % (data_path))
 
+    # Load the left, center and right images that come from the simulator,
+    # these are the training features.
     print("Loading training images:")
     left_images = load_images(drive_log['left'], data_path)
     print("left [%s, %s, %s, %s]" % (left_images.shape))
@@ -23,6 +26,14 @@ def build_model(model_path, data_path):
     print("center [%s, %s, %s, %s]" % (center_images.shape))
     right_images = load_images(drive_log['right'], data_path)
     print("right [%s, %s, %s, %s]" % (right_images.shape))
+
+    # Load the training labels.
+    steering = drive_log['steering']
+    throttle = drive_log['throttle']
+    brake = drive_log['brake']
+    speed = drive_log['speed']
+
+
 
 
 if __name__ == '__main__':
