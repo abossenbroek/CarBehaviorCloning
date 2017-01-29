19bb8b38d5be9a72f49d7d2b6e9e2903eb6d403d
Anton Bossenbroek <anton.bossenbroek@me.com>
2017-01-29 16:40:26 +0100

    First try at building a model.

--- model.py
+++ model.py
@@ -1,8 +1,12 @@
 import argparse
-# import keras
 import numpy as np
 import pandas as pd
 from skimage import io
+
+from keras.models import Sequential
+from keras.layers import Convolution2D, Model
+from keras.layers import Activation, Dropout, Flatten, Dense, Input, Merge, MaxPooling2D
+
 
 def load_images(files, data_path):
     def load_func(files):
@@ -10,7 +14,7 @@ def load_images(files, data_path):
             img = io.imread('%s/%s' % (data_path, fl.strip()))
             yield img
 
-    return np.stack(load_func(files), axis=0)
+    return np.stack(load_func(files), axis=-1)
 
 
 def build_model(model_path, data_path):
@@ -33,6 +37,34 @@ def build_model(model_path, data_path):
     brake = drive_log['brake']
     speed = drive_log['speed']
 
+    model = Sequential()
+    left_input = Convolution2D(32, 3, 3, border_mode='same',
+                               activation='relu',
+                               input_shape=left_images.shape[0:3])
+    right_input = Convolution2D(32, 3, 3, border_mode='same',
+                                activation='relu',
+                                input_shape=right_images.shape[0:3])
+    center_input = Convolution2D(32, 3, 3, border_mode='same',
+                                 activation='relu',
+                                 input_shape=center_images.shape[0:3])
+
+    x = Merge([left_input, center_input, right_input], mode='concat')
+    x = Convolution2D(32, 3, 3, activation='relu')(x)
+    x = MaxPooling2D(pool_size=(2, 2))(x)
+    x = model.add(Dropout(0.5))(x)
+
+    steering_output = Dense(1, activation='linear', name='steering_output')(x)
+    throttle_output = Dense(1, activation='linear', name='throttle_output')(x)
+    brake_output = Dense(1, activation='linear', name='brake_output')(x)
+    speed_output = Dense(1, activation='linear', name='speed_output')(x)
+
+    model = Model(input=[left_input, center_input, right_input],
+                  output=[steering_output, throttle_output,
+                          brake_output, speed_output])
+    model.compile(optimizer='rmsprop', loss='mean_squared_error')
+    model.fit([left_images, center_images, right_images],
+               [steering, throttle, brake, speed],
+              nb_epochs=50, batch_size=100)
 
 
 
@@ -44,5 +76,4 @@ if __name__ == '__main__':
                         help='Path to data that should be used to train model')
     args = parser.parse_args()
     build_model(args.model, args.data)
-
 
