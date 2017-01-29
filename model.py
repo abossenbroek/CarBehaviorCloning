1c303018fb980b6e2fd0afef4f47b4b2acdf18e3
Anton Bossenbroek <anton.bossenbroek@me.com>
2017-01-29 19:07:13 +0100

    Updated model to normalize input layers.

--- model.py
+++ model.py
@@ -2,21 +2,29 @@ import argparse
 import numpy as np
 import pandas as pd
 from skimage import io
+from skimage import transform
 
 from keras.models import Sequential, Model
 from keras.layers import Convolution2D
-from keras.layers import Activation, Dropout, Flatten, Dense, Input, merge, MaxPooling2D
+from keras.layers import Activation, Dropout, Flatten, Dense, Input, Merge
+from keras.layers import MaxPooling2D, merge
+from keras.layers.normalization import BatchNormalization
 
 
 def load_images(files, data_path):
     def load_func(files):
         for fl in files:
-            img = io.imread('%s/%s' % (data_path, fl.strip()))
+            img = transform.downscale_local_mean(io.imread('%s/%s' %
+                                                 (data_path, fl.strip())),
+                                       factors=(4,4,1))
             yield img
 
-    images = np.stack(load_func(files[0:400]), axis=-1)
-    return images.reshape([images.shape[3], images.shape[2],
+    images = np.stack(load_func(files[0:100]), axis=-1)
+    images = images.reshape([images.shape[3], images.shape[2],
                            images.shape[0], images.shape[1]])
+    images = images.astype('float32')
+    images = images/256.0
+    return images
 
 
 def build_model(model_path, data_path):
@@ -34,40 +42,50 @@ def build_model(model_path, data_path):
     print("right [%s, %s, %s, %s]" % (right_images.shape))
 
     # Load the training labels.
-    steering = drive_log['steering']
-    throttle = drive_log['throttle']
-    brake = drive_log['brake']
-    speed = drive_log['speed']
+    steering = drive_log['steering'][0:100]
+    throttle = drive_log['throttle'][0:100]
+    brake = drive_log['brake'][0:100]
+    speed = drive_log['speed'][0:100]
 
+    left_img = Input(shape=(3, 40, 80), dtype='float32', name="left_img")
+    left_input = BatchNormalization()(left_img)
+    left_input = Convolution2D(32, 5, 5, border_mode='same')(left_input)
+    left_input = Activation('relu')(left_input)
+    left_input = Dropout(0.5)(left_input)
+    right_img = Input(shape=(3, 40, 80), dtype='float32', name="right_img")
+    right_input = BatchNormalization()(right_img)
+    right_input = Convolution2D(32, 5, 5, border_mode='same')(right_input)
+    right_input = Activation('relu')(right_input)
+    right_input = Dropout(0.5)(right_input)
+    center_img = Input(shape=(3, 40, 80), dtype='float32', name="center_img")
+    center_input = BatchNormalization()(center_img)
+    center_input = Convolution2D(32, 5, 5, border_mode='same')(center_input)
+    center_input = Activation('relu')(center_input)
+    center_input = Dropout(0.5)(center_input)
 
-    left_input = Convolution2D(32, 3, 3, border_mode='same',
-                               activation='relu',
-                               input_shape=left_images.shape[1:])
-    right_input = Convolution2D(32, 3, 3, border_mode='same',
-                                activation='relu',
-                                input_shape=right_images.shape[1:])
-    center_input = Convolution2D(32, 3, 3, border_mode='same',
-                                 activation='relu',
-                                 input_shape=center_images.shape[1:])
-
-    #x = merge([left_input, center_input, right_input], mode='concat')
-    x = Convolution2D(32, 3, 3, activation='relu')(left_input)
-    x = MaxPooling2D(pool_size=(2, 2))(x)
+    x = merge([left_input, center_input, right_input], mode='concat')
+    x = Convolution2D(3, 5, 5, border_mode='same', activation='relu')(x)
+    x = Convolution2D(64, 5, 5, border_mode='same', activation='relu')(x)
+    x = MaxPooling2D((3, 3), strides=(1,1), border_mode='same')(x)
+    x = Dropout(0.5)(x)
+    x = Convolution2D(64, 3, 3, border_mode='same', activation='relu')(x)
+    x = Convolution2D(64, 5, 5, border_mode='same', activation='relu')(x)
+    x = MaxPooling2D((3, 3), strides=(1,1), border_mode='same')(x)
     x = Dropout(0.5)(x)
 
-    steering_output = Dense(1, activation='linear', name='steering_output')(x)
+    x = Flatten()(x)
+    steering_output = Dense(1, activation='sigmoid', name='steering_output')(x)
     throttle_output = Dense(1, activation='linear', name='throttle_output')(x)
     brake_output = Dense(1, activation='linear', name='brake_output')(x)
     speed_output = Dense(1, activation='linear', name='speed_output')(x)
 
-    #model = Model(input=[left_input, center_input, right_input],
-    model = Model(input=left_input,
+    model = Model(input=[left_img, center_img, right_img],
                   output=[steering_output, throttle_output,
                           brake_output, speed_output])
     model.compile(optimizer='rmsprop', loss='mean_squared_error')
-    model.fit([left_images, center_images, right_images],
-               [steering, throttle, brake, speed],
-              nb_epochs=50, batch_size=100)
+    model.fit(x=[left_images, center_images, right_images],
+               y=[steering, throttle, brake, speed],
+              nb_epoch=50, batch_size=100)
 
 
 
