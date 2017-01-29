d643303c0ee0355b41483a0a212ead644672dbfd
Anton Bossenbroek <anton.bossenbroek@me.com>
2017-01-29 19:44:47 +0100

    Fully working training model.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # CarBehaviorCloning
 Behavior cloning of driving a car on a track
+
+## Background literature
+[NVidia's article on a pipeline](http://images.nvidia.com/content/tegra/automotive/images/2016/solutions/pdf/end-to-end-dl-using-px.pdf)
--- model.py
+++ model.py
@@ -9,6 +9,7 @@ from keras.layers import Convolution2D
 from keras.layers import Activation, Dropout, Flatten, Dense, Input, Merge
 from keras.layers import MaxPooling2D, merge
 from keras.layers.normalization import BatchNormalization
+from keras.layers.advanced_activations import PReLU
 
 
 def load_images(files, data_path):
@@ -16,7 +17,7 @@ def load_images(files, data_path):
         for fl in files:
             img = transform.downscale_local_mean(io.imread('%s/%s' %
                                                  (data_path, fl.strip())),
-                                       factors=(4,4,1))
+                                       factors=(2,2,1))
             yield img
 
     images = np.stack(load_func(files[0:100]), axis=-1)
@@ -27,7 +28,7 @@ def load_images(files, data_path):
     return images
 
 
-def build_model(model_path, data_path):
+def build_model(model_path, data_path, epochs):
     # Load the log file.
     drive_log = pd.read_csv("%s/driving_log.csv" % (data_path))
 
@@ -47,33 +48,39 @@ def build_model(model_path, data_path):
     brake = drive_log['brake'][0:100]
     speed = drive_log['speed'][0:100]
 
-    left_img = Input(shape=(3, 40, 80), dtype='float32', name="left_img")
+    left_img = Input(shape=(3, 80, 160), dtype='float32', name="left_img")
     left_input = BatchNormalization()(left_img)
     left_input = Convolution2D(32, 5, 5, border_mode='same')(left_input)
-    left_input = Activation('relu')(left_input)
-    left_input = Dropout(0.5)(left_input)
-    right_img = Input(shape=(3, 40, 80), dtype='float32', name="right_img")
+    left_input = PReLU()(left_input)
+    left_input = Dropout(0.25)(left_input)
+    right_img = Input(shape=(3, 80, 160), dtype='float32', name="right_img")
     right_input = BatchNormalization()(right_img)
     right_input = Convolution2D(32, 5, 5, border_mode='same')(right_input)
-    right_input = Activation('relu')(right_input)
-    right_input = Dropout(0.5)(right_input)
-    center_img = Input(shape=(3, 40, 80), dtype='float32', name="center_img")
+    right_input = PReLU()(right_input)
+    right_input = Dropout(0.25)(right_input)
+    center_img = Input(shape=(3, 80, 160), dtype='float32', name="center_img")
     center_input = BatchNormalization()(center_img)
     center_input = Convolution2D(32, 5, 5, border_mode='same')(center_input)
-    center_input = Activation('relu')(center_input)
-    center_input = Dropout(0.5)(center_input)
+    center_input = PReLU()(center_input)
+    center_input = Dropout(0.25)(center_input)
 
     x = merge([left_input, center_input, right_input], mode='concat')
-    x = Convolution2D(3, 5, 5, border_mode='same', activation='relu')(x)
-    x = Convolution2D(64, 5, 5, border_mode='same', activation='relu')(x)
-    x = MaxPooling2D((3, 3), strides=(1,1), border_mode='same')(x)
-    x = Dropout(0.5)(x)
-    x = Convolution2D(64, 3, 3, border_mode='same', activation='relu')(x)
-    x = Convolution2D(64, 5, 5, border_mode='same', activation='relu')(x)
-    x = MaxPooling2D((3, 3), strides=(1,1), border_mode='same')(x)
+    x = Convolution2D(3, 5, 5, border_mode='same')(x)
+    x = PReLU()(x)
+    x = Convolution2D(24, 5, 5, border_mode='same')(x)
+    x = PReLU()(x)
+    x = Convolution2D(36, 5, 5, border_mode='same')(x)
+    x = PReLU()(x)
+    x = Convolution2D(48, 3, 3, border_mode='same')(x)
+    x = PReLU()(x)
+    x = Convolution2D(64, 3, 3, border_mode='same')(x)
+    x = PReLU()(x)
     x = Dropout(0.5)(x)
 
     x = Flatten()(x)
+    x = Dense(100)(x)
+    x = Dense(50)(x)
+    x = Dense(10)(x)
     steering_output = Dense(1, activation='sigmoid', name='steering_output')(x)
     throttle_output = Dense(1, activation='linear', name='throttle_output')(x)
     brake_output = Dense(1, activation='linear', name='brake_output')(x)
@@ -82,11 +89,24 @@ def build_model(model_path, data_path):
     model = Model(input=[left_img, center_img, right_img],
                   output=[steering_output, throttle_output,
                           brake_output, speed_output])
-    model.compile(optimizer='rmsprop', loss='mean_squared_error')
+    model.compile(optimizer='adam', loss='mean_squared_error')
     model.fit(x=[left_images, center_images, right_images],
                y=[steering, throttle, brake, speed],
-              nb_epoch=50, batch_size=100)
+              nb_epoch=epochs, batch_size=100)
 
+    model_json_file = "%s/model.json" % (model_path)
+    model_weights_file = "%s/model.h5" % (model_path)
+    print("About to save model to '%s'" % (model_json_file))
+    print("About to save model weights to '%s'" % (model_weights_file))
+
+
+    model_json = model.to_json()
+    with open(model_json_file, "w") as json_file:
+        json_file.write(model_json)
+
+    print("Succesfully saved JSON file")
+    model.save_weights(model_weights_file)
+    print("Succesfully saved weights")
 
 
 if __name__ == '__main__':
@@ -95,6 +115,9 @@ if __name__ == '__main__':
                         help='Path to model definition json. Model weights should be on the same path.')
     parser.add_argument('--data', dest='data', type=str,
                         help='Path to data that should be used to train model')
+    parser.add_argument('--epochs', dest='epochs', type=int,
+                        help='Path to data that should be used to train model')
+
     args = parser.parse_args()
-    build_model(args.model, args.data)
+    build_model(args.model, args.data, args.epochs)
 
