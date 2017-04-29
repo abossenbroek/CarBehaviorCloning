359652d53cac4ffadd254b2893afad3d5e799f35
Anton Bossenbroek <anton.bossenbroek@me.coml>
2017-04-29 15:47:57 +0000

    calibrated model to newest data

--- train_model.sh
+++ train_model.sh
@@ -2,8 +2,13 @@ python3 model.py --model . --data data/ --epochs 150 2>&1 | tee train_model.txt 
   git add train_model.txt && \
   cp weights.hdf5 model.h5 && \
   git commit -a -m "trained new model" &&
-  python3 model.py --model . --data data/ --epochs 150 --load model.json 2>&1 | tee refine_model.txt && \
+  python3 model.py --model . --data car/ --epochs 150 --load model.json 2>&1 | tee refine_model.txt && \
   git add refine_model.txt && \
   cp weights.hdf5 model.h5 && \
-  git commit -a -m "trained new model" 
+  git commit -a -m "trained new model" && git push \ 
+  python3 model.py --model . --data car3/ --epochs 150 --load model.json 2>&1 | tee refine_model.txt && \
+  git add refine_model.txt && \
+  cp weights.hdf5 model.h5 && \
+  git commit -a -m "trained new model" && git push
+
 
