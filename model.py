9f92587370f3da82d25a6afc77efc7507ce160f1
Anton Bossenbroek <anton.bossenbroek@me.coml>
2017-04-29 08:38:22 +0000

    Changed error measure back to mse from RMSE

--- model.py
+++ model.py
@@ -222,7 +222,7 @@ def build_model(model_path, data_path, epochs, new_data=MISSING,
 
     print(model.summary())
 
-    model.compile(optimizer='adamax', loss='rmse')
+    model.compile(optimizer='adamax', loss='mse')
 
     early_stopping = EarlyStopping(monitor='val_loss', patience=10)
     checkpointer = ModelCheckpoint(filepath="./weights.hdf5", verbose=1,
